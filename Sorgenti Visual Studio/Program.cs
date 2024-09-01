e4c26ec181171e0c9f7a87f88f85f97cf050db51
Andrea Barbadoro <60655085+Zorban62@users.noreply.github.com>
2024-09-01 09:14:17 +0200

    Versione 2.15

---
+++ Sorgenti Visual Studio/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Andrea_NameSpace
+{
+    static class Program
+    {
+        /// <summary>
+        /// Punto di ingresso principale dell'applicazione.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new DashBoardEmulEP());
+        }
+    }
+
+
+    //-----------------------------------------------------------------------------------------------------------------
+    // Metodi di estensione per gli Array
+    //-----------------------------------------------------------------------------------------------------------------
+    public static class Estensione_Array
+    {
+        /// <summary>  Controlla se i due Array sono identici </summary>
+        public static bool ValEqual(this Array a, Array b)
+        {
+            if (a.Length == b.Length)
+            {
+                int i;
+                for (i = 0; i < a.Length; i++)
+                {
+                    if (!a.GetValue(i).Equals(b.GetValue(i)))
+                    {
+                        break;
+                    }
+                }
+                if (i == a.Length) return true;
+            }
+            return false;
+        }
+
+        /// <summary> Confronta il primo Array con il secondo a partire da ind del secondo </summary>
+        public static bool ValEqual(this Array a, Array b, long ind)
+        {
+            int i;
+            for (i = 0; i < a.Length; i++)
+            {
+                if (!a.GetValue(i).Equals(b.GetValue(i + ind)))
+                {
+                    break;
+                }
+            }
+            if (i == a.Length) return true;
+            else return false;
+        }
+
+    }
+
+}
---
+++ Sorgenti Visual Studio/WatcherFile.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Media;
+using System.IO;
+using System.Windows.Forms;
+
+
+//WatcherFile WF = new WatcherFile(this, File_Path_Hex, LoadFileHex);
+
+namespace Andrea_NameSpace
+{
+    class WatcherFile
+    {
+        FileSystemWatcher fswHex;
+        private string File_Path;
+        
+        public delegate void ReLoadFile(string File);
+        private ReLoadFile Reload_File;
+        
+        public WatcherFile(object obj, string F_Path, ReLoadFile RLF)
+        {
+            Reload_File = RLF;
+            File_Path = F_Path;
+            fswHex = new FileSystemWatcher();
+            fswHex.Path = Path.GetDirectoryName(File_Path);
+            fswHex.SynchronizingObject = (System.ComponentModel.ISynchronizeInvoke)obj; //Sincronizza il watcher con questo oggetto (thread oggetto)
+            fswHex.NotifyFilter = NotifyFilters.LastWrite;
+            fswHex.Filter = Path.GetFileName(File_Path);
+            fswHex.Changed += new FileSystemEventHandler(HexChange);
+            fswHex.EnableRaisingEvents = true; //Abilita il controllo LastWrite 
+        }
+
+        private void HexChange(object source, FileSystemEventArgs e)
+        {
+            fswHex.EnableRaisingEvents = false; //Blocca il secondo evento di FileChange (seconda scrittura per modificare gli attributi)
+            //SystemSounds.Beep.Play();
+            //monitor.ClearMonitor();
+            if (File.Exists(File_Path))
+            {
+                //monitor.ScriviSuMonitor("File Modificato ultima scrittura {0}, ricarico file", File.GetLastWriteTime(Nome_File).ToString("HH:mm:ss:FFF"));
+                //monitor.ScriviSuMonitor("File {0} Modificato ultimo accesso {1}", File_Path_Hex, File.GetLastAccessTime(File_Path_Hex).ToString("HH:mm:ss:FFF"));
+
+                DialogResult result = MessageBox.Show("File " + " modificato vuoi rileggerlo?", "File Change", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                if (result == System.Windows.Forms.DialogResult.Yes) Reload_File(File_Path);
+                fswHex.EnableRaisingEvents = true; //Ri-Abilita il controllo LastWrite
+            }
+            //else monitor.ScriviSuMonitor("Il file {0} non esiste più", File_Path_Hex);
+        }
+
+    }
+}
